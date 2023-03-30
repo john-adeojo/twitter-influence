50dc422e749dfdad973f0c0b61facbe490492bed
john-adeojo <johnadeojo91@gmail.com>
2023-03-30 13:16:01 +0100

    added analysis
    added chi square stats, standardized residuals

--- scripts/clustering/.ipynb_checkpoints/cluster-checkpoint.py
+++ scripts/clustering/.ipynb_checkpoints/cluster-checkpoint.py
@@ -7,23 +7,24 @@ import plotly.graph_objects as go
 
 
 class ClusterAnalysis:
-    def __init__(self, dataframe, n_neighbors=15, min_cluster_size=5, min_dist=0.1):
+    def __init__(self, dataframe, n_neighbors=15, min_cluster_size=5, min_dist=0.1, metric='euclidean'):
         self.dataframe = dataframe.copy()
         self.n_neighbors = n_neighbors
         self.min_cluster_size = min_cluster_size
         self.min_dist = min_dist
+        self.metric = metric
 
     def perform_umap(self):
-        reducer = UMAP(n_neighbors=self.n_neighbors, min_dist=self.min_dist, random_state=42)
+        reducer = UMAP(n_neighbors=self.n_neighbors, min_dist=self.min_dist, metric=self.metric, random_state=42)
         umap_data = reducer.fit_transform(self.dataframe[['favorite_count_pf_mean', 'retweet_count_pf_mean', 'quote_count_pf_mean','reply_count_pf_mean']])
         self.dataframe['x'] = umap_data[:, 0]
         self.dataframe['y'] = umap_data[:, 1]
 
     def perform_hdbscan(self):
         np.random.seed(42)
-        clusterer = HDBSCAN(min_cluster_size=self.min_cluster_size)
+        clusterer = HDBSCAN(min_cluster_size=self.min_cluster_size, metric=self.metric)
         self.dataframe['cluster'] = clusterer.fit_predict(self.dataframe[['x', 'y']])
-        self.dataframe.to_csv(r"C:\Users\johna\anaconda3\envs\twitter-influence-env\twitter-influence\data\02_intermediate\tweet_analysis_data.csv")
+        self.dataframe.to_csv(r"C:\Users\johna\anaconda3\envs\twitter-influence-env\twitter-influence\data\02_intermediate\tweet_analysis_data.csv", index=False)
 
     # def plot_scatter(self):
     #     fig = px.scatter(self.dataframe, x='x', y='y', color='cluster', hover_name=self.dataframe['name'], opacity=0.4)
@@ -39,7 +40,7 @@ class ClusterAnalysis:
                                      marker=dict(size=6, opacity=0.4), hovertext=cluster_data['name'],
                                      text=cluster_data['name'], textposition='top center', textfont=dict(size=10, color='black')))
 
-        fig.update_layout(showlegend=True, width=500, height=500)
+        fig.update_layout(title='Influence Clusters', showlegend=True, width=500, height=500)
         fig.show()
 # hovertext=cluster_data['name']
 
